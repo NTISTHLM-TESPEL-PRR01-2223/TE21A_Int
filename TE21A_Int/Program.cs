dc75a0f985d9698b58758a90c0002384c932ff08
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-09-13 10:35:06 +0200

    Initial commit

---
+++ TE21A_Int/Program.cs
@@ -0,0 +1,37 @@
+﻿
+// string number = "44" + "76";
+
+// Console.WriteLine(number);
+
+// string name = "Micke";
+
+// bool s = 6 > 3;
+
+// if (s)
+// {
+//   Console.WriteLine("YES");
+// }
+
+int potionHealing = 10;
+int curseDamage = 10;
+
+int hp = 50 + 50;
+
+hp = hp + potionHealing;
+
+hp += potionHealing;
+
+hp++;
+hp--;
+
+hp -= curseDamage;
+
+Console.WriteLine(hp);
+
+
+if (hp > 0)
+{
+  Console.WriteLine("Jag lever!");
+}
+
+Console.ReadLine();
